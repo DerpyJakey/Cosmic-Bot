1e8732aab79380bbe2d0beb0c43bb33c9772ddb0
derpyjakey <sjacob16s@gmail.com>
2016-03-30 11:09:20 -0500

    Removed Directories. Currently fixing things

--- src/main/java/CosmicBot.java
+++
@@ -1,9 +0,0 @@
-import frame.InitFrame;
-import frame.ClientFrame;
-
-public class CosmicBot {
-	public static void main(String[] args) {
-		InitFrame initFrame = new InitFrame();
-		ClientFrame clientFrame = new ClientFrame();
-	}
-}
