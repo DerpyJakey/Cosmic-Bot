6253e46dd21909d0a4bd0cfcaadb602fedacebe5
DerpyJakey <sjacob16s@gmail.com>
2016-05-21 08:26:06 -0500

    Added some components. Now it's time for work.

--- com/org/derpyjakey/frames/ClientFrame.java
+++ com/org/derpyjakey/frames/ClientFrame.java
@@ -9,6 +9,9 @@ import com.org.derpyjakey.utilities.Language;
 public class ClientFrame {
 	String current_Language;
 	JFrame client_Frame;
+	JTextField client_Chat_Text_Field;
+	JTextField client_Input_Text_Field;
+	JButton client_Send_Button;
 	
 	public ClientFrame() {
 		initialize();
@@ -20,10 +23,14 @@ public class ClientFrame {
 	void initialize() {
 		current_Language = "";
 		client_Frame = new JFrame();
+		client_Chat_Text_Field = new JTextField();
+		client_Input_Text_Field = new JTextField();
+		client_Send_Button = new JButton();s
 	}
 	
 	void updateLanguage() {
-	
+		client_Frame.setTitle(Language.getText("Frame.Client"));
+		client_Send_Button.setText(Language.getText("Button.Send"));
 	}
 	
 	void addComponents() {
